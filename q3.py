d85fb57dfbfe4bf1bbaac317b2101eb892be8811
Arsalan Arish <mail@arsalanarish.com>
2026-02-05 00:14:24 +0500

    Fixed q3 and made 'PGC' to print horizontally

--- q3.py
+++ q3.py
@@ -5,33 +5,42 @@
 #        *        *    *   *
 #        *        ******   ******
 
-# Print P
+# Now to make them all horizontal, we will collect the first lines printed by all loops into a single string
+# Same for the other lines
+# Then just print the strings sequentially
+
+
+lines_P = []
 for i in range(5):
+    line = ""
     for j in range(6):
         if (i == 0 or i == 2) or (i == 1 and (j == 0 or j == 5)) or (i > 2 and j == 0):
-            print("*", end="")
+            line += "*"
         else:
-            print(" ", end="")
-    print()
+            line += " "
+    lines_P.append(line)
 
-print()
-
-# Print G
+lines_G = []
 for i in range(5):
+    line = ""
     for j in range(6):
         if (i == 0 or i == 4) or (i == 1 and j == 0) or (i == 2 and (j == 0 or j > 2)) or (i == 3 and (j == 0 or j == 5)):
-            print("*", end="")
+            line += "*"
         else:
-            print(" ", end="")
-    print()
+            line += " "
+    lines_G.append(line)
 
-print()
-
-# Print C
+lines_C = []
 for i in range(5):
+    line = ""
     for j in range(6):
         if (i == 0 or i == 4) or ((i > 0 or i < 4) and (j == 0)):
-            print("*", end="")
+            line += "*"
         else:
-            print(" ", end="")
-    print()
+            line += " "
+    lines_C.append(line)
+
+
+print("\n")
+for p in range(5): # 5 is the number of lines to print vertically
+    print(lines_P[p] + "   " + lines_G[p] + "   " + lines_C[p])
